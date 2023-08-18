b06c0b9f399f17febbdffeb2284548ab7a013f79
akhalinem <akhalinem@gmail.com>
2023-08-18 10:37:09 +0500

    Setup mlagents-custom

---
+++ Agents/mlagents-custom/main.py
@@ -0,0 +1,9 @@
+from mlagents_envs.environment import UnityEnvironment
+
+# This is a non-blocking call that only loads the environment.
+env = UnityEnvironment(file_name=None, seed=1, side_channels=[])
+
+# Start interacting with the environment.
+env.reset()
+
+behavior_names = env.behavior_specs.keys()
